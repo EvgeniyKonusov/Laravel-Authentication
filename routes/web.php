22c972a588baf5d69a38812d09a440a029594658
EvgeniyKonusov <Konusovevgeniy@gmail.com>
2024-10-12 20:29:18 +0500

    pattern

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('register', [UserController::class, 'create'])->name('register');
+Route::post('register', [UserController::class, 'store'])->name('user.store');
+Route::get('login', [UserController::class, 'login'])->name('login');
