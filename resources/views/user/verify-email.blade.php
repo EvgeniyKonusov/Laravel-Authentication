4def5a3ea7df9e6eef426e2e820fc04fd8dbd197
EvgeniyKonusov <Konusovevgeniy@gmail.com>
2024-10-14 15:42:49 +0500

    email verification

---
+++ resources/views/user/verify-email.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('title', 'Home page')
+
+@section('content')
+    
+    <div class="alert alert-info" role="alert">
+        Благодарим вас за регистрацию! Ссылка для подтверждения вашей регистрации была отправлена на ваш электронный адрес.
+    </div>
+    <div>
+        Не получили ссылку?
+        <form method="POST" action="">
+            @csrf
+            <button type="submit" class="btn btn-link ps-0">Send link</button>
+        </form>
+    </div>
+
+@endsection
